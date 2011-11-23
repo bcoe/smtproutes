f2a4beddc91065fa19c98e8742a8146e759b9765
bcoe <bencoe@gmail.com>
2011-11-23 12:31:45 -0800

    Removed my email address from the gmail_spf_auth.

--- smtproutes/sender_auth/gmail_spf_auth.py
+++ smtproutes/sender_auth/gmail_spf_auth.py
@@ -1,11 +1,8 @@
 import spf
 
 class GmailSPFAuth(object):
-    
-    ARBITRARY_GMAIL_ADDRESS = 'bencoe@gmail.com'
-    
     def auth(self, message_data=None, peer_ip=None, message=None):
-        result_status = spf.check(i=peer_ip, s=self.ARBITRARY_GMAIL_ADDRESS, h='google.com')[0]
+        result_status = spf.check(i=peer_ip, s='@gmail.com', h='google.com')[0]
         if 'pass' in result_status:
             return True
         return False
