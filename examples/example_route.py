46658410691e139a0c9474cd8270bdc82deddbfe
bcoe <bencoe@gmail.com>
2011-11-23 11:55:54 -0800

    Updated example to reflect our different sender authentication approaches.

--- examples/example_route.py
+++ examples/example_route.py
@@ -1,8 +1,30 @@
 from smtproutes import Route
+from smtproutes.sender auth import DKIMAuth, GmailSPFAuth, SPFAuth
 
 class ExampleRoute(Route):
     
-    def echo_route(self, route=r'(?P<prefix>[^@]*)@(?P<suffix>.*)'):
+    def open_route(self, route=r'(?P<prefix>open)@(?P<suffix>.*)'):
+        print "%s at %s sent the message: \n\n %s" % (
+            self.prefix,
+            self.suffix,
+            self.message
+        )
+
+    def dkim_route(self, route=r'(?P<prefix>dkim)@(?P<suffix>.*)', sender_auth=DKIMAuth):
+        print "%s at %s sent the message: \n\n %s" % (
+            self.prefix,
+            self.suffix,
+            self.message
+        )
+
+    def spf_route(self, route=r'(?P<prefix>spf)@(?P<suffix>.*)', sender_auth=SPFAuth):
+        print "%s at %s sent the message: \n\n %s" % (
+            self.prefix,
+            self.suffix,
+            self.message
+        )
+
+    def spf_google_apps_spf_route(self, route=r'(?P<prefix>spf)@(?P<suffix>.*)', sender_auth=[SPFAuth, GmailSPFAuth]):
         print "%s at %s sent the message: \n\n %s" % (
             self.prefix,
             self.suffix,
