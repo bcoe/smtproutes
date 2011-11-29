8b78ea5fad54b7880e025bdfca678094dd2aba9d
bcoe <bencoe@gmail.com>
2011-11-29 13:15:23 -0800

    By upgrading secure-smtp to v1.1.1 we get huge performance improvements.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="smtproutes",
-    version="1.1.1",
+    version="1.1.2",
     description="A simple, Sinatra inspired, SMTP routing server.",
     author="Benjamin Coe",
     author_email="bencoe@gmail.com",
     url="https://github.com/bcoe/smtproutes",
     packages = find_packages(),
     install_requires = [
-        'secure-smtpd>=1.0.0',
+        'secure-smtpd>=1.1.1',
         'pydkim',
         'dnspython',
         'pyspf',
