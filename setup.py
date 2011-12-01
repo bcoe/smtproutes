95e86190abef59de12b11719515b75debcbab6cc
bcoe <bencoe@gmail.com>
2011-12-01 10:54:12 -0800

    v1.2.3 fixed some problems in the underlying secure-smtpd server.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="smtproutes",
-    version="1.2.2",
+    version="1.2.3",
     description="A simple, Sinatra inspired, SMTP routing server.",
     author="Benjamin Coe",
     author_email="bencoe@gmail.com",
     url="https://github.com/bcoe/smtproutes",
     packages = find_packages(),
     install_requires = [
-        'secure-smtpd>=1.1.4',
+        'secure-smtpd>=1.1.5',
         'pydkim',
         'dnspython',
         'pyspf',
