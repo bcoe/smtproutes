2401b7fd92d6274ae13ed6d179d46b0a61563405
bcoe <bencoe@gmail.com>
2011-11-30 16:09:25 -0800

    v1.2.0 added better support for attachments on messages. Added better logging.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="smtproutes",
-    version="1.1.2",
+    version="1.2.0",
     description="A simple, Sinatra inspired, SMTP routing server.",
     author="Benjamin Coe",
     author_email="bencoe@gmail.com",
     url="https://github.com/bcoe/smtproutes",
     packages = find_packages(),
     install_requires = [
-        'secure-smtpd>=1.1.1',
+        'secure-smtpd>=1.1.2',
         'pydkim',
         'dnspython',
         'pyspf',
