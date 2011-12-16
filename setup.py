6009dcefc05daa17ae531bd0b1914590099b0f98
bcoe <bencoe@gmail.com>
2011-12-15 17:45:05 -0800

    Updated version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smtproutes",
-    version="1.2.6",
+    version="1.2.7",
     description="A simple, Sinatra inspired, SMTP routing server.",
     author="Benjamin Coe",
     author_email="bencoe@gmail.com",
