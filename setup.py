44acb9e00d18428c739911328353be48a214122c
bcoe <bencoe@gmail.com>
2011-12-22 17:00:51 -0800

    Updated secure-smtpd dependency to fix CPU bug.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="smtproutes",
-    version="1.2.8",
+    version="1.2.9",
     description="A simple, Sinatra inspired, SMTP routing server.",
     author="Benjamin Coe",
     author_email="bencoe@gmail.com",
     url="https://github.com/bcoe/smtproutes",
     packages = find_packages(),
     install_requires = [
-        'secure-smtpd>=1.1.7',
+        'secure-smtpd>=1.1.8',
         'pydkim',
         'dnspython',
         'pyspf',
